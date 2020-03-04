f307959ebc839111153cb3a6f3c772fa3ebb0478
Mark Ronald Torio <47067649+Emm18@users.noreply.github.com>
2020-03-03 16:27:56 -0800

    Codewars daily code challenge (03/03/2020)

---
+++ SudokuSolutionValidator.js
@@ -0,0 +1,112 @@
+//https://www.codewars.com/kata/529bf0e9bdf7657179000008
+
+// Sudoku Background
+// Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid with digits from 1 to 9, 
+// so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks) contain all of the digits from 1 to 9.
+// (More info at: http://en.wikipedia.org/wiki/Sudoku)
+
+// Sudoku Solution Validator
+// Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, 
+// and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which 
+// will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+
+// The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+
+// Examples
+// validSolution([
+//   [5, 3, 4, 6, 7, 8, 9, 1, 2],
+//   [6, 7, 2, 1, 9, 5, 3, 4, 8],
+//   [1, 9, 8, 3, 4, 2, 5, 6, 7],
+//   [8, 5, 9, 7, 6, 1, 4, 2, 3],
+//   [4, 2, 6, 8, 5, 3, 7, 9, 1],
+//   [7, 1, 3, 9, 2, 4, 8, 5, 6],
+//   [9, 6, 1, 5, 3, 7, 2, 8, 4],
+//   [2, 8, 7, 4, 1, 9, 6, 3, 5],
+//   [3, 4, 5, 2, 8, 6, 1, 7, 9]
+// ]); // => true
+// validSolution([
+//   [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+//   [6, 7, 2, 1, 9, 0, 3, 4, 8],
+//   [1, 0, 0, 3, 4, 2, 5, 6, 0],
+//   [8, 5, 9, 7, 6, 1, 0, 2, 0],
+//   [4, 2, 6, 8, 5, 3, 7, 9, 1],
+//   [7, 1, 3, 9, 2, 4, 8, 5, 6],
+//   [9, 0, 1, 5, 3, 7, 2, 1, 4],
+//   [2, 8, 7, 4, 1, 9, 6, 3, 5],
+//   [3, 0, 0, 4, 8, 1, 1, 7, 9]
+// ]); // => false
+
+
+
+function validSolution(board) {
+
+  //check if the board contains 0 
+  for(let i = 0; i <= board.length - 1; i++){
+    if(board[i].includes(0)){
+      return false;
+    }
+  }
+
+  //get all rows
+  let rows = board.map(row => row);
+
+  //get all columns
+  let cols = [];
+  for (let i = 0; i <= board[0].length - 1; i++) {
+    let tempArr = [];
+    for (let j = 0; j <= board.length - 1; j++) {
+      tempArr.push(board[j][i]);
+    }
+    cols.push(tempArr)
+  }
+
+  //get all 3x3 boxes
+  let box3x3 = [];
+  let startingRow = 0;
+  let startingCol = 0;
+  while (startingRow < rows.length - 1) {
+    let tempCol = startingCol;
+    while (tempCol <= rows[0].length - 1) {
+      let tempArr = [];
+      let tempRow = startingRow;
+      let initialCol = tempCol;
+      while (tempRow <= startingRow + 2) {
+        for (let j = tempCol; j <= tempCol + 2; j++) {
+          tempArr.push(board[tempRow][j]);
+        }
+        tempCol = initialCol;
+        tempRow += 1;
+      }
+      tempRow = startingRow;
+      tempCol += 3;
+      box3x3.push(tempArr);
+    }
+    startingRow += 3;
+  }
+
+  //check rows, cols, and 3x3
+  if(!checkOneToNine(rows) && !checkOneToNine(cols) && !checkOneToNine(box3x3)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkOneToNine(arr) {
+  let validator = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let foundInvalid = false;
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (!foundInvalid) {
+      validator.map(num => {
+        if (arr[i].filter(val => val == num).length > 1) {
+          foundInvalid = true;
+        }
+      });
+    } else {
+      break;
+    }
+  }
+
+  return foundInvalid;
+}
