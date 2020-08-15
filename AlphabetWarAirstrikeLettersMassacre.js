ba112b25536a7d6fca85bc8f2de78249811c0c6a
Mark Ronald Torio <47067649+Emm18@users.noreply.github.com>
2020-08-14 20:57:50 -0700

    Codewars daily code challenge (08/10/2020)

---
+++ AlphabetWarAirstrikeLettersMassacre.js
@@ -0,0 +1,63 @@
+//https://www.codewars.com/kata/5938f5b606c3033f4700015a
+
+//Introduction
+//There is a war and nobody knows - the alphabet war! There are two groups of hostile letters. 
+//The tension between left side letters and right side letters was too high and the war began. 
+//The letters called airstrike to help them in war - dashes and dots are spreaded everywhere on the battlefield.
+
+//Task
+//Write a function that accepts fight string consists of only small letters and * which means a 
+//bomb drop place. Return who wins the fight after bombs are exploded. When the left side wins 
+//return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+//The left side letters and their power:
+
+// w - 4
+// p - 3 
+// b - 2
+// s - 1
+//The right side letters and their power:
+
+// m - 4
+// q - 3 
+// d - 2
+// z - 1
+//The other letters don't have power and are only victims.
+//The * bombs kills the adjacent letters ( i.e. aa*aa => a___a, **aa** => ______ );
+
+//Example
+//alphabetWar("s*zz");           //=> Right side wins!
+//alphabetWar("*zd*qm*wp*bs*"); //=> Let's fight again!
+//alphabetWar("zzzz*s*");       //=> Right side wins!
+//alphabetWar("www*www****z");  //=> Left side wins!
+
+function alphabetWar(fight){
+  let newFight = fight.split('');
+  let bombIndex = newFight.indexOf('*');
+  let leftSide = {'w':4,'p':3,'b':2,'s':1};
+  let rightSide = {'m':4,'q':3,'d':2,'z':1};
+  let leftScore = 0;
+  let rightScore = 0;
+  
+  while(newFight.indexOf('*') > -1){    
+    newFight[bombIndex] = '';
+    if(newFight[bombIndex - 1] && newFight[bombIndex - 1] != '*'){
+      newFight[bombIndex - 1] = '';
+    }; 
+    if(newFight[bombIndex + 1]  && newFight[bombIndex + 1] != '*'){
+      newFight[bombIndex + 1] = '';
+    };
+    bombIndex = newFight.indexOf('*');
+  };
+  
+  newFight.map(v => {
+    if(leftSide[v]){
+      leftScore += parseInt(leftSide[v]);
+    };
+    if(rightSide[v]){
+      rightScore += parseInt(rightSide[v]);
+    };
+  });
+  
+  return leftScore > rightScore ? 'Left side wins!' : leftScore < rightScore ? 'Right side wins!' : "Let's fight again!";
+};
