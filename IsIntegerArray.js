5a1f3032c44719dcee388f54d2882dddd28e98bf
Mark Ronald Torio <47067649+Emm18@users.noreply.github.com>
2020-08-29 13:13:11 -0700

    Codewars daily code challenge (08/25/2020)

---
+++ IsIntegerArray.js
@@ -0,0 +1,28 @@
+//https://www.codewars.com/kata/52a112d9488f506ae7000b95
+
+//Write a function with the signature shown below:
+
+//function isIntArray(arr) {
+//  return true
+//}
+//returns true / True if every element in an array is an integer or a float with no decimals.
+//returns true / True if array is empty.
+//returns false / False for every other input.
+
+function isIntArray(arr) {
+  if(Array.isArray(arr) && arr.length == 0){
+    return true;
+  };
+  
+  if(!arr){
+    return false;
+  };
+
+  for(let i = 0; i <= arr.length - 1; i++){
+    if(arr[i] !== parseInt(arr[i])){
+      return false;
+    };
+  };
+  
+  return true;
+};
