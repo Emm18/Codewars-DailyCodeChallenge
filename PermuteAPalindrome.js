a098d51ba20a2a129d466a37ac5a2139d2323e14
Mark Ronald Torio <47067649+Emm18@users.noreply.github.com>
2020-09-10 01:40:33 -0700

    Codewars daily code challenge (09/06/2020)

---
+++ PermuteAPalindrome.js
@@ -0,0 +1,33 @@
+//https://www.codewars.com/kata/58ae6ae22c3aaafc58000079
+
+//Write a function that will check whether the permutation of an input string is a palindrome. 
+//Bonus points for a solution that is efficient and/or that uses only built-in language functions. 
+//Deem yourself brilliant if you can come up with a version that does not use any function whatsoever.
+
+//Example
+//madam -> True
+//adamm -> True
+//junk -> False
+
+//Hint
+//The brute force approach would be to generate all the permutations of the string and check each one of 
+//them whether it is a palindrome. However, an optimized approach will not require this at all.
+
+function permuteAPalindrome (input) { 
+  let newInput = input.split('');
+  let oddCtr = 0;
+  
+  while(newInput.length > 0){
+    let letter = newInput.splice(0,1)[0];
+    let temp = newInput.filter(x => x != letter);
+    if(((newInput.length - temp.length) + 1) % 2 == 1){
+      oddCtr++;
+    };
+    newInput = temp;
+    if(oddCtr > 1){
+      return false;
+    };
+  };
+  
+  return true;
+};
