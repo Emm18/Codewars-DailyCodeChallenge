05cf2631dc5486da9dcfdecbf62672cb4c878ad7
Mark Ronald Torio <47067649+Emm18@users.noreply.github.com>
2020-12-16 23:23:15 -0800

    Codewars daily code challenge (12/16/2020)

---
+++ MovingZerosToTheEnd.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/52597aa56021e91c93000cb0
+
+//Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+//moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+var moveZeros = function (arr) {
+  let shouldBeLast = ['0', 0];
+  let arrWithoutZeroes = arr.filter(x => !shouldBeLast.includes(x));
+  let moveToLast = arr.filter(x => shouldBeLast.includes(x));
+  
+  return [...arrWithoutZeroes, ...moveToLast];
+};
